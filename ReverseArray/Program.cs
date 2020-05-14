017ff399a3937fd0476740bbac372740ca3720a3
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-14 16:16:14 +0200

    Add project files.

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            Console.WriteLine("Enter 5 intergers and press enter after each");
+            _ = int.TryParse(Console.ReadLine(), out a[0]);
+            _ = int.TryParse(Console.ReadLine(), out a[1]);
+            _ = int.TryParse(Console.ReadLine(), out a[2]);
+            _ = int.TryParse(Console.ReadLine(), out a[3]);
+            _ = int.TryParse(Console.ReadLine(), out a[4]);
+
+            Console.WriteLine("your array is:");
+            Console.WriteLine("[{0}]", string.Join(", ", a));//print Array in a style
+           
+            Array.Reverse(a);  //reverse Array
+            Console.WriteLine("your reversed array is");
+            Console.WriteLine("[{0}]", string.Join(", ", a));
+
+        }
+    }
+}
